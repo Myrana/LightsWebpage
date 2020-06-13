350086f224aca03e707e127b7403c79eec2d74b1
Amanda Winslow <66683190+Myrana@users.noreply.github.com>
2020-06-13 10:16:32 -0500

    Update TestPage.php
    hoping this fixes the conflicts

--- Winslow Lights/TestPage.php
+++ Winslow Lights/TestPage.php
@@ -332,7 +332,7 @@ brightnessSlider.oninput = function()
 	<input type="checkbox" name="clearFinish">
 		<p>
 		<label for="On">Power On</label>
-	<input type="checkbox" name="lights" value="ON">	
+	<input type="checkbox" name="powerOn" value="ON">	
 		</p>
 	
 		<p><button type="submit" name="LightShow">Send Show</button></p>
