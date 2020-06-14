2143ec9bbccf02b91e32e1129004129d2cffbda7
Amanda Winslow <66683190+Myrana@users.noreply.github.com>
2020-06-13 19:26:03 -0500

    Update TestPage.php

--- Winslow Lights/TestPage.php
+++ Winslow Lights/TestPage.php
@@ -408,7 +408,7 @@ $conn->close();
 		</select>	
 		<p>
 			<label>New Playlist Name*</label> <br />
-			<input type="text" id="PlayListNameId" name="PlaylistName" max="50" placeholder="Enter a playlist name (50 characters)" >
+			<input type="text" id="PlayListNameId" name="PlaylistName" max="50" placeholder="Enter a playlist name (50 characters)">
 			</p>	
 		
 		<p>
