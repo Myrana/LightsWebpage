10d3467ab99dbb625f86b178879e95ecfdedd061
Amanda Winslow <66683190+Myrana@users.noreply.github.com>
2020-06-15 12:16:26 -0500

    did the cart
    added the shopping cart

---
+++ Winslow Lights/shopIndex.php
@@ -0,0 +1,103 @@
+<?php
+include('CommonFunctions.php');
+$status="";
+if (isset($_POST['code']) && $_POST['code']!=""){
+$code = $_POST['code'];
+$result = mysqli_query(
+$con,
+"SELECT * FROM products WHERE code='$code'"
+);
+$row = mysqli_fetch_assoc($result);
+$name = $row['name'];
+$code = $row['code'];
+$price = $row['price'];
+$image = $row['image'];
+ 
+$cartArray = array(
+ $code=>array(
+ 'name'=>$name,
+ 'code'=>$code,
+ 'price'=>$price,
+ 'quantity'=>1,
+ 'image'=>$image)
+);
+ 
+if(empty($_SESSION["shopping_cart"])) {
+    $_SESSION["shopping_cart"] = $cartArray;
+    $status = "<div class='box'>Product is added to your cart!</div>";
+}else{
+    $array_keys = array_keys($_SESSION["shopping_cart"]);
+    if(in_array($code,$array_keys)) {
+ $status = "<div class='box' style='color:red;'>
+ Product is already added to your cart!</div>"; 
+    } else {
+    $_SESSION["shopping_cart"] = array_merge(
+    $_SESSION["shopping_cart"],
+    $cartArray
+    );
+    $status = "<div class='box'>Product is added to your cart!</div>";
+ }
+ 
+ }
+}
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Shop</title>
+<script src="https://kit.fontawesome.com/4717f0a393.js" crossorigin="anonymous"></script>
+<link href="Styles.css" rel="stylesheet" type="text/css">	
+</head>
+
+<script>
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
+</script>
+<body>
+    <div w3-include-html="Nav.html"></div>
+
+<script>
+includeHTML();
+</script>
+	
+<?php
+if(!empty($_SESSION["shopping_cart"])) {
+$cart_count = count(array_keys($_SESSION["shopping_cart"]));
+?>
+<div class="cart_div">
+<a href="cart.php"><img src="cart-icon.png" /> Cart<span>
+<?php echo $cart_count; ?></span></a>
+</div>
+<?php
+}
+?>	
+	
+</body>
+</html>
