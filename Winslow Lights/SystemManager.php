f26836b06b9c5d941890bb38d0d3aabd300abd8b
Amanda Winslow <66683190+Myrana@users.noreply.github.com>
2020-06-25 15:01:14 -0500

    Surprise!
    I did a thing :)

--- Winslow Lights/SystemManager.php
+++ Winslow Lights/SystemManager.php
@@ -428,55 +428,10 @@ $conn->close();
 
 
     </form>
-		<button id="btnEditlist">Edit Shows</button>
+		<button type="button" id="btnEditlist" onClick="window.location.href='editShows.php'">Edit Shows</button>
     </div>
     </div>
 	
-	<div id="myModal" class="modal">
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-<div class="column"><label for="dad">Dad's Test select</label>
-	<select id="dad" name="dad">
-	<option id="dadd" value = "dad">Have fun!</option>
-	
-	</select>
-	  
-	  
-	  </div>
-        <?php include('showDesigner.php'); ?>
-
-    </div>
-	</div>
-
-<script>
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("btnEditlist");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-</script>
 	
 </body>
 </html>
---
+++ Winslow Lights/editShows.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Edit Shows</title>
+<script src="https://kit.fontawesome.com/4717f0a393.js" crossorigin="anonymous"></script>
+<link href="Styles.css" rel="stylesheet" type="text/css">	
+</head>
+
+<body>
+	<?php include('Nav.php'); ?>
+	
+	<div class="column" style="margin-top: 15px;"><label for="dad">Dad's Test select</label>
+	<select id="dad" name="dad">
+	<option id="dadd" value = "dad">Have fun!</option>
+	
+	</select>
+	  
+	  
+	  </div>
+        <?php include('showDesigner.php'); ?>
+
+    </div>
+	</div>
+</body>
+</html>
