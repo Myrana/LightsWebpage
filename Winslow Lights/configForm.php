370616c50116307c51215f3ba5d29eeb7c91df39
Amanda Winslow <66683190+Myrana@users.noreply.github.com>
2020-07-12 08:57:36 -0500

    Update configForm.php
    testing https

--- Winslow Lights/configForm.php
+++ Winslow Lights/configForm.php
@@ -295,7 +295,7 @@ $conn->close();
 
 ?>
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script type="text/javascript">
     $(function () {
         $("#motionFeature").click(function () {
