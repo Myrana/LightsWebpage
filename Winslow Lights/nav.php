8bd130ab1b2be8139615f553e8dc8b8c3992cd1b
Amanda Winslow <66683190+Myrana@users.noreply.github.com>
2020-07-17 16:28:52 -0500

    Update nav.php
    forgot FAQ has latin too

--- Winslow Lights/nav.php
+++ Winslow Lights/nav.php
@@ -4,7 +4,7 @@
 	<nav>
 	  <ul><a href="index.php" title="Home" ><i class="fas fa-home" style="color: #34eb86"></i></a></ul>
 	  <ul><a href="gallery.php" title="Gallery" style="color: cornflowerblue"><i class="fas fa-images"></i></a></ul>
-	  <ul><a href="FAQ.php" title="Frequently Asked Questions" style="color: burlywood"><i class="fas fa-question-circle"></i></a></ul>	
+	  <!--<ul><a href="FAQ.php" title="Frequently Asked Questions" style="color: burlywood"><i class="fas fa-question-circle"></i></a></ul>-->	
 	  <ul><a href="shopIndex.php" title="Shop" style="color: deeppink"><i class="fas fa-shopping-bag"></i></a></ul>	
 	  <ul><a href="configForm.php" title="Configuration"><i class="fas fa-cogs" style="color: #ff3399"></i></a></ul>
 	  <ul><a href="registration.php" title="Registration"><i class="fas fa-user-plus" style="color: #ffff00"></i></a></ul>
