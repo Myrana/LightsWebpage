58d2769a0e78c4906f37ef41bb5641ddac343177
hellweek <hellweek@RomoServer.local>
2020-07-26 13:11:35 -0500

    Matrix Art Yes!

--- lumaWinSystem/matrixArt.php
+++ lumaWinSystem/matrixArt.php
@@ -29,13 +29,20 @@ if(isset($_REQUEST['btnWorkMatrix']))
 	{
 		$row = mysqli_fetch_array($results);
 		$ledNumber = 0;
-		$numLeds = $row['stripRows'] * $row['stripColumns'];
-		for($ledRow = 0; $ledRow < $row['stripRows']; $ledRow++)
+		$ledRows = $row['stripRows'];
+		$ledColumns = $row['stripColumns'];
+		if($ledRows == 1)
 		{
-			for($ledColumn = 0; $ledColumn < $row['stripColumns']; $ledColumn++)
+			 $ledRows = $ledColumns;
+			 $ledColumns = 1;
+		}
+		
+		for($ledRow = 0; $ledRow < $ledRows; $ledRow++)
+		{
+			for($ledColumn = 0; $ledColumn < $ledColumns; $ledColumn++)
 			{
 				$ledNumber += 1;
-				$matrixHTML .= "<span id='" . $ledNumber . "' onClick='getId()' class='pixel'></span>";
+				$matrixHTML .= "<span id='" . $ledNumber . "' onClick='getId()' class='pixel'></span>";			
 				
 			}	
 			$matrixHTML .= "<br>";
@@ -45,6 +52,14 @@ if(isset($_REQUEST['btnWorkMatrix']))
 	
 }
 
+if(isset($_REQUEST['btnDisplayArt']))
+{
+	if(!empty($_POST['matrixData']))
+	{
+		
+		sendMQTT(getServerHostName($_SESSION["LightSystemID"]), $_POST['matrixData']);
+	}	
+}
 
 $lightSystemsoption = '';
 $lightSystemsScript = '';
@@ -106,8 +121,19 @@ background-color: red;
 
 </head>
 
+<?php 
+	if(!empty($_REQUEST) && !isset($_REQUEST['btnDisplayArt']))
+	{
+		echo '<body onload="setMatrixColors();" >';
+	}
+	else
+	{
+		echo '<body>';
+	}
+	
+?>
 
-<body >
+
 <?php include("nav.php");  ?>
 
 
@@ -118,9 +144,92 @@ background-color: red;
 function getId()
 {	
 	var pixel = document.getElementById(this.event.target.id);
-	pixel.style.background = 'red';
+	var color = document.getElementById('colorSelect');
+	
+	pixel.style.background = color.value;
+	
 }
 
+function setMatrixColors()
+{
+	
+	var pixel;
+	var systemNameId = document.getElementById("SystemNameId");
+	var baseColor = document.getElementById("baseColor");
+    var index = parseInt(systemNameId.value);
+    var system = systemsMap.get(index);
+    var ledNum = 0;
+    
+    for(var row = 1; row < system.stripRows; row++)
+    {
+		
+		for(var column = 0; row < system.stripColumns; column++)
+		{
+			ledNum += 1;
+			pixel = document.getElementById(ledNum);
+			pixel.style.background = baseColor.value;
+			
+			
+		}	
+	}
+
+}
+function componentFromStr(numStr, percent) {
+    var num = Math.max(0, parseInt(numStr, 10));
+    return percent ?
+        Math.floor(255 * Math.min(100, num) / 100) : Math.min(255, num);
+}
+
+function rgbToHex(rgb) {
+    var rgbRegex = /^rgb\(\s*(-?\d+)(%?)\s*,\s*(-?\d+)(%?)\s*,\s*(-?\d+)(%?)\s*\)$/;
+    var result, r, g, b, hex = "";
+    if ( (result = rgbRegex.exec(rgb)) ) {
+        r = componentFromStr(result[1], result[2]);
+        g = componentFromStr(result[3], result[4]);
+        b = componentFromStr(result[5], result[6]);
+
+        hex = "0x" + (0x1000000 + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+    return hex;
+}
+
+function storeMatrix()
+{
+	var pixel;
+	var systemNameId = document.getElementById("SystemNameId");
+	var matrixData = document.getElementById("matrixData");
+	
+    var index = parseInt(systemNameId.value);
+    var system = systemsMap.get(index);
+    var numLeds = system.stripRows * system.stripColumns;
+    var ledNum = 0;
+    var colNum = 0;
+    
+    var matrixJson = '{"show": "23","pixles": {';
+	  
+	for(var row = 1; row <= system.stripRows; row++)
+    {
+	  
+		for(var column = 0; column < system.stripColumns; column++)
+		{
+			ledNum += 1;
+			pixel = document.getElementById(ledNum);
+			matrixJson += '"' + ledNum + '":{"r":' + row + ',"c":' + column + ',"co":"' + rgbToHex(pixel.style.backgroundColor) + '"}';
+			if(column != (system.stripColumns - 1))
+				matrixJson += ",";
+		
+		}
+		
+		if(row != system.stripRows)
+			matrixJson += ",";	
+	}
+
+	matrixJson += '}}';
+	
+	matrixData.value = matrixJson;
+		
+	
+}
 
 </script>
 
@@ -136,7 +245,7 @@ function getId()
         </select>       
     </p>
 	<p><button type="submit" name="btnWorkMatrix">Create Art!</button>
-	<p><button type="submit" onClick='storeMatrix()' name="btnDisplayArt" >Display Art!</button>
+	<p><button type="submit" onClick="storeMatrix()" name="btnDisplayArt" >Display Art!</button>
 
     </div>
 
@@ -148,7 +257,8 @@ function getId()
 			<input type="color" id="baseColor" name="baseColor" value="#ADD8E6" />
 			<input type="checkbox" id="reset" name="reset" /><label>reset</label>
 			<input type="color" id="colorSelect" name="colorSelect" value="#34ebde" />
-		<?php echo $matrixHTML; ?>
+			<input type="text" id="matrixData" name="matrixData" hidden />
+		<p></p><?php echo $matrixHTML; ?></P>
 		
 		</div>
 		
