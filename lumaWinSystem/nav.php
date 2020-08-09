0a6ddca86200903e4c7d82f74134fb0b96e72be0
hellweek <hellweek@RomoServer.local>
2020-08-08 19:44:55 -0500

    Made Config Page part of admin priv.

--- lumaWinSystem/nav.php
+++ lumaWinSystem/nav.php
@@ -3,10 +3,14 @@
 <center><img src="Images/winslowBanner.png" alt="LumaWin LED Systems"></center>
 	<nav>
 	  <ul><a href="registration.php" title="Registration"><i class="fas fa-user-plus" style="color: #FF0004"></i></a></ul>  
-	  <ul><a href="configForm.php" title="Configuration"><i class="fas fa-cogs" style="color: #F97400"></i></a></ul>
+	   <?php if($_SESSION['isAdmin'] == 1) 
+		echo '<ul><a href="configForm.php" title="Configuration"><i class="fas fa-cogs" style="color: #F97400"></i></a></ul>';
+	  ?>
+	  
 	  <ul><a href="lightShows.php" title="Light Shows"><i class="fas fa-network-wired" style="color: #FFFD00"></i></a></ul>
 	  <ul><a href="editShows.php" title="Edit Shows"><i class="fas fa-edit" style="color: #225900"></i></a></ul>
-	 <ul><a href="editArt.php" title="Edit Art"><i class="fa fa-palette" style="color: #0200D4"></i></a></ul>
+	  <ul><a href="editArt.php" title="Edit Art"><i class="fa fa-palette" style="color: #0200D4"></i></a></ul>
+	 
 	 <?php if($_SESSION['isAdmin'] == 1) 
 		echo '<ul><a href="testPage.php" title="Test Page" ><i class="fas fa-exclamation-circle" style="color: #500887"></i></a></ul>';
 	 ?>
