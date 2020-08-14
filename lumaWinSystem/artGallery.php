d34c1fc6b5a55800d4aa3aeb856c6ff2c9f25d45
hellweek <hellweek@RomoServer.local>
2020-08-14 11:59:53 -0500

    User Art Gallery working, needs cleaning

--- lumaWinSystem/artGallery.php
+++ lumaWinSystem/artGallery.php
@@ -1,6 +1,31 @@
 <!doctype html>
 <?php 
 include('header.php'); 
+
+include_once('commonFunctions.php');
+
+if($_SESSION['authorized'] == 0)
+{
+  header("Location: registration.php");
+  exit();
+}
+
+$target_dir = "userArt/";
+   
+
+$files = scandir($target_dir, 1);
+$imageList = "";
+foreach($files as $file)
+{
+	if(is_file($target_dir . $file ) == true)
+		$imageList .= '<img src="' . $target_dir . $file . '" width="320" height="160" />';
+    
+    
+}
+home/hellweek/code/uploadArt
+//print_r($files);
+
+
 ?>
 
 <?php include("nav.php");  ?>
@@ -8,6 +33,8 @@ include('header.php');
 
 <body>
 	
+<?php echo $imageList;?>
+
 <!--<img src=”image location” width=”1920” height=”1017” />	-->
 	
 </body>
