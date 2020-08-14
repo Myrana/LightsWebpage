86401702060ab1939172b24d91fe603d9dbd2ae5
Amanda Winslow <66683190+Myrana@users.noreply.github.com>
2020-08-14 09:35:23 -0500

    now there is a button

--- lumaWinSystem/nav.php
+++ lumaWinSystem/nav.php
@@ -10,6 +10,7 @@
 	  <ul><a href="lightShows.php" title="Light Shows"><i class="fas fa-network-wired" style="color: #FFFD00"></i></a></ul>
 	  <ul><a href="editShows.php" title="Edit Shows"><i class="fas fa-edit" style="color: #225900"></i></a></ul>
 	  <ul><a href="editArt.php" title="Edit Art"><i class="fa fa-palette" style="color: #0200D4"></i></a></ul>
+	  <ul><a href="artGallery.php" title="Art Gallery"><i class="far fa-images" style="color: #82CD1E"></i></a></ul>	
 	 
 	 <?php if($_SESSION['isAdmin'] == 1) 
 		echo '<ul><a href="testPage.php" title="Test Page" ><i class="fas fa-exclamation-circle" style="color: #500887"></i></a></ul>';
